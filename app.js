67adbcbf1fd964e8e273ea1d430e7a2ab1dd6153
shellcode777 <supertank777@gmail.com>
2022-08-16 01:51:58 +0900

    kakao share

---
+++ app.js
@@ -0,0 +1,12 @@
+if (!Kakao.isInitialized()) {
+    Kakao.init('c54b7bbce7b6fa59b01e11e86eb0e414');
+  }
+  
+var sendKakao = function() {
+    // 메시지 공유 함수
+    Kakao.Link.sendScrap({
+        requestUrl: 'https://shellcode777.github.io/hh_invitation/'
+    // 페이지 url
+    });
+}
+
